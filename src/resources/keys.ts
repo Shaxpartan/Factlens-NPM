87f3bd1c35176adbb0edc1f6616417c7ea40529a
Shaxpartan <95138238+Shaxpartan@users.noreply.github.com>
2026-08-17 14:26:12 +0600

    feat: expose key customization resource

--- src/resources/keys.ts
+++ src/resources/keys.ts
@@ -1,12 +1,17 @@
 import { FactLensConfigurationError } from "../errors.js";
 import type { HttpTransport } from "../http.js";
 import type { ApiKey, CreatedApiKey, KeyCreateOptions, KeyListOptions, KeyRevokeOptions, RequestOptions } from "../types/index.js";
+import { CustomizationResource } from "./customization.js";
 
 export class KeysResource {
+  readonly customization: CustomizationResource;
+
   constructor(
     private readonly transport: HttpTransport,
     private readonly selectedProject: () => string | undefined,
-  ) {}
+  ) {
+    this.customization = new CustomizationResource(transport, selectedProject);
+  }
 
   async list(input: KeyListOptions = {}, options?: RequestOptions): Promise<ApiKey[]> {
     const projectId = this.projectId(input.projectId);
